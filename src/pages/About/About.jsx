97466d0e26ac4e05a2f3b4e14a59268f7f191e28
Medokyn <imangali_medet@mail.ru>
2024-02-17 19:26:19 +0600

    1

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,7 +2,7 @@ import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Info from "../About/about-info/info"
 
-// import employers from "../../components/Images/man-about.png"
+
 
 function About(){
   return(
